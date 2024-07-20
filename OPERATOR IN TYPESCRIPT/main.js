1d33d143f7a9925cfe7bd4e90b5963f75d61acd1
SOMANcode786 <somanamir43@gmail.com>
2024-07-20 02:56:29 -0700

    Add files via upload

---
+++ OPERATOR IN TYPESCRIPT/main.js
@@ -0,0 +1,17 @@
+// OPEARTOR IN TYPESCRIPT
+// ADDITON
+var firstNumber = 56;
+var secondNumber = 45;
+console.log(firstNumber + secondNumber);
+// SUBTRACTION
+var Number1 = 56;
+var Number2 = 45;
+console.log(Number1 - Number2);
+// MUTILPLICATION
+var Num1 = 56;
+var Num2 = 45;
+console.log(Num1 * Num2);
+// DIVISION
+var div1 = 56;
+var div2 = 45;
+console.log(div1 / div2);
---
+++ OPERATOR IN TYPESCRIPT/main.ts
@@ -0,0 +1,26 @@
+// OPEARTOR IN TYPESCRIPT
+
+// ADDITON
+
+let firstNumber:number=56;
+let secondNumber:number=45;
+
+console.log(firstNumber+secondNumber);
+
+// SUBTRACTION
+let Number1:number=56;
+let Number2:number=45;
+
+console.log(Number1-Number2);
+
+// MUTILPLICATION
+let Num1:number=56;
+let Num2:number=45;
+
+console.log(Num1*Num2);
+
+// DIVISION
+let div1:number=56;
+let div2:number=45;
+
+console.log(div1/div2);
